74aba5ef69f588570d26305d2fc262c3dec9f75b
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:51:52 -0200

    Create scenarios.h

---
+++ scenarios.h
@@ -0,0 +1,22 @@
+/*
+ * scenarios.h
+ *
+ *  Created on: 16 de out de 2017
+ *      Author: Alexandre
+ */
+
+#ifndef SCENARIOS_H_
+#define SCENARIOS_H_
+
+/// Functions ///
+
+void BurstScenario();
+void VacuumScenario();
+void VIRTScenario();
+void REALScenario();
+void UXUScenario();
+void GRAVScenario();
+void SEEDScenario();
+void ElectronScenario();
+
+#endif /* SCENARIOS_H_ */
