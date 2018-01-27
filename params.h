e2b3ff2f65101a01fe46f2a9f6d928489fc7668b
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:46:09 -0200

    Update params.h

--- params.h
+++ params.h
@@ -10,27 +10,41 @@
 
 // Automaton dimensions
 
-#define ORDER 	4
-#define SIDE 	(1 << ORDER)			// Size of the universal cube
-#define DIAMETER ((SIDE - 1) * 2)
-#define NDIR 	6						// spatial directions (von Neumann convention)
-#define LIGHT (p1 % (3 * SIDE) == 0)	// light step
+#define ORDER 		4
+#define SIDE 		(1<<ORDER)				// size of the universal cube
+#define NPREONS		(16)
+#define DIAMETER 	((SIDE-1)*2)
+#define NDIR 		6						// spatial directions (von Neumann convention)
 
-// Other input parameters
+// Derived parameters
+
+#define MOD2		(SIDE*SIDE/4)
+#define BURST		(3*SIDE/2+1)
+#define SYNCH		(2*DIAMETER+BURST)
+
+// PWM parameters
+
+#define STEP		((int)log2(SIDE))
+#define NSTEPS		(SIDE/STEP)
+
+// Arbitrary parameters
 
 #define HBAR		2
-#define LOST		(log2(SIDE))
-#define RAMP		4				// universal case: 10^10
+#define RAMP		4						// universal case: 10^10
 
 // Powers
 
-#define SIDE2	(SIDE*SIDE)
-#define SIDE3	(SIDE*SIDE*SIDE)
-#define SIDE4	(SIDE*SIDE*SIDE*SIDE)
+#define SIDE2		(SIDE*SIDE)
+#define SIDE3		(SIDE2*SIDE)
+#define SIDE4		(SIDE3*NPREONS)
 
-// PWM
+// Wrapping constants
 
-#define STEP	((int)log2(SIDE))
-#define NSTEPS	(SIDE / LIGHT)
+#define WRAP0       ((SIDE3-SIDE2)*NPREONS)
+#define WRAP1  		(SIDE2*NPREONS)
+#define WRAP2      	((SIDE2-SIDE)*NPREONS)
+#define WRAP3     	(SIDE*NPREONS)
+#define WRAP4      	((SIDE-1)*NPREONS)
+#define WRAP5      	(NPREONS)
 
 #endif /* PARAMS_H_ */
