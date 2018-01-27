7c69b577c848ba4440d1bbdd1ab23c383dd78af6
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:40:26 -0200

    Create common.h

---
+++ common.h
@@ -0,0 +1,28 @@
+/*
+ * common.h
+ *
+ *  Created on: 8 de out de 2017
+ *      Author: Alexandre
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#define true 1
+#define false 0
+#define null 0
+
+#define WIDTH   800
+#define HEIGHT  800
+
+// Colors
+
+#define BLK     0
+#define WHT     1
+#define RR		2
+#define GG		3
+#define BB		4
+#define GRAY    5
+#define BOX		6
+
+#endif /* COMMON_H_ */
