e12e3b2d4690334d238013e2b8d1f24637f00346
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:37:28 -0200

    Update automaton.h

--- automaton.h
+++ automaton.h
@@ -8,9 +8,24 @@
 #ifndef AUTOMATON_H_
 #define AUTOMATON_H_
 
+#include <pthread.h>
 #include "tile.h"
-#define true 1
-#define false 0
-#define null 0
+
+extern Tile *pri0;
+extern unsigned long timer;
+//extern char voxels[], _voxels[];
+extern pthread_mutex_t mutex;
+extern char imgbuf[3][SIDE3];
+extern char *draft, *clean;
+
+/// Functions ///
+
+void *AutomatonLoop();
+void DeleteAutomaton();
+void delay(unsigned int mseconds);
+int sgn(int n);
+Tile *getNual(int dir);
+void reemit();
+void burst(boolean cp);
 
 #endif /* AUTOMATON_H_ */
