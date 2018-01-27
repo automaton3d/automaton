69fb68fb4e8d7a502f1826eb9197aeffbcb33c49
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:56:23 -0200

    Create tree.h

---
+++ tree.h
@@ -0,0 +1,19 @@
+/*
+ * tree.h
+ *
+ *  Created on: 9 de abr de 2017
+ *      Author: Alexandre
+ */
+
+#ifndef TREE_H_
+#define TREE_H_
+
+#include "tuple.h"
+
+extern const Tuple dirs[];
+
+/// Functions ///
+
+boolean isAllowed(int dir, Tuple p, unsigned char d0);
+
+#endif /* TREE_H_ */
