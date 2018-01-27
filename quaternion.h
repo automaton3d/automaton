c701442c8105c8db67a2fd0a3c85281eb83d279e
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-01-27 11:49:51 -0200

    Create quaternion.h

---
+++ quaternion.h
@@ -0,0 +1,24 @@
+/*
+ * quaternion.h
+ *
+ *  Created on: 7 de mai de 2017
+ *      Author: Alexandre
+ */
+
+#ifndef QUATERNION_H_
+#define QUATERNION_H_
+
+typedef struct
+{
+	double x, y, z, w;
+
+} Quaternion;
+
+
+void conjugate(Quaternion q0, Quaternion q1);
+void normalise(Quaternion q0);
+void scaleQ(Quaternion q0, double s);
+void mul(Quaternion *q0, Quaternion q1, Quaternion q2);
+void add(Quaternion q0, Quaternion q1, Quaternion q2);
+
+#endif /* QUATERNION_H_ */
