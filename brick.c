9188ccfff831b2ead816f39ca64f534da2706439
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-03-22 23:00:00 -0300

    Update brick.c

--- brick.c
+++ brick.c
@@ -12,18 +12,22 @@
 void copyBrick(Brick *dst, Brick *org)
 {
 	Tuple p0 = dst->p0;
+	unsigned p18 = dst->p18;
 	unsigned short w = dst->p19;
 	*dst = *org;
 	dst->p0 = p0;
+	dst->p18 = p18;
 	dst->p19 = w;
 }
 
 void cleanBrick(Brick *t)
 {
 	Tuple p0 = t->p0;
+	unsigned p18 = t->p18;
 	unsigned short w = t->p19;
 	memset(t, 0, sizeof(Brick));
 	t->p0 = p0;
+	t->p18 = p18;
 	t->p19 = w;
 }
 
