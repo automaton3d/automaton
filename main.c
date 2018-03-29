10180de8dc42b86aa10bcdf7c4ec494ea98c7aef
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-03-28 21:44:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 /*
  * main3d.c
- *
- *  Created on: 4 de mar de 2017
- *      Author: Alexandre
  */
 
-#include "main3d.h"
+#include "main.h"
+
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -16,6 +14,7 @@
 #include "automaton.h"
 #include "mouse.h"
 #include "quaternion.h"
+#include "text.h"
 
 LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 pthread_t loop, display;
@@ -25,6 +24,8 @@ Vector3d p0, p1;
 
 Quaternion q, qstart;
 DWORD* pixels;
+
+boolean splash = true;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -54,11 +55,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE,
 		500, 20, WIDTH + 4, HEIGHT + 4, NULL, NULL, hInstance, NULL);
  	free(title);
-	//
- 	pthread_create(&loop, NULL, &AutomatonLoop, NULL);
- 	sleep(1);
- 	pthread_create(&display, NULL, &DisplayLoop, NULL);
- 	sleep(1);
+ 	//
+ 	ShowWindow (hwnd, nShowCmd);
+ 	UpdateWindow (hwnd) ;
  	//
 	while(GetMessage(&msg, hwnd, 0,0))
 		DispatchMessage(&msg);
@@ -72,7 +71,7 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		case WM_CREATE:
     		hdc = GetDC(hwnd);
     		pixels = (DWORD*) malloc(WIDTH * HEIGHT * sizeof(DWORD));
-    		initPlot(pixels);
+    		//
 			ZeroMemory(&bmInfo, sizeof(BITMAPINFO));
 			bmInfo.bmiHeader.biBitCount    = 32;
 			bmInfo.bmiHeader.biHeight      = HEIGHT;
@@ -81,9 +80,11 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			bmInfo.bmiHeader.biWidth       = WIDTH;
 			bmInfo.bmiHeader.biCompression = BI_RGB;
 			//
-			myBitmap = CreateCompatibleBitmap(GetDC(hwnd), WIDTH, HEIGHT);
-			myCompatibleDC = CreateCompatibleDC(GetDC(hwnd));
-			SelectObject(myCompatibleDC, myBitmap);
+			myBitmap = CreateCompatibleBitmap(hdc, WIDTH, HEIGHT);
+			dc = CreateCompatibleDC(hdc);
+			SelectObject(dc, myBitmap);
+			//
+		 	pthread_create(&display, NULL, &DisplayLoop, NULL);
 			break;
 
     	case WM_ERASEBKGND:
@@ -92,7 +93,7 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		case WM_DESTROY:
 			DeleteAutomaton();
     		DeleteObject(myBitmap) ;
-    	    DeleteDC(myCompatibleDC);
+    	    DeleteDC(dc);
     	    DeleteDC(hdc);
 			PostQuitMessage(0);
 			exit(0);
@@ -113,6 +114,27 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			break;
 
 		case WM_KEYDOWN:
+			if(splash)
+			{
+				switch(wparam)
+				{
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+						scenario = (wparam & 0x0f) - 1;
+						initPlot(pixels);
+						pthread_create(&loop, NULL, &AutomatonLoop, NULL);
+						sleep(1);
+						splash = false;
+						break;
+				}
+				break;
+			}
 			switch(wparam)
 			{
 				case 'A':
@@ -152,6 +174,17 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 					norm3d(&direction);
 					break;
 				case '1':
+					direction.x = 0;
+					direction.y = 0;
+					direction.z = -1;
+					attitude.x = 1;
+					attitude.y = 0;
+					attitude.z = 0;
+					position.x = 0;
+					position.y = 0;
+					position.z = 1.5 * GRID;
+					break;
+				case '2':
 					direction.x = -1;
 					direction.y = 0;
 					direction.z = 0;
@@ -162,7 +195,7 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 					position.y = 0;
 					position.z = 0;
 					break;
-				case '2':
+				case '3':
 					direction.x = 0;
 					direction.y = -1;
 					direction.z = 0;
@@ -172,17 +205,6 @@ LRESULT CALLBACK MyWndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 					position.x = 0;
 					position.y = 1.5 * GRID;
 					position.z = 0;
-					break;
-				case '3':
-					direction.x = 0;
-					direction.y = 0;
-					direction.z = -1;
-					attitude.x = 1;
-					attitude.y = 0;
-					attitude.z = 0;
-					position.x = 0;
-					position.y = 0;
-					position.z = 1.5 * GRID;
 					break;
 			}
 			break;
@@ -202,5 +224,4 @@ void DeleteAutomaton()
 	free(pri0);
 	free(dual0);
 }
-
 
