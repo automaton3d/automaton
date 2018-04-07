ec76b6573f774daca7ac838029f4f0f816c186bc
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-04-06 21:52:39 -0300

    Create mouse.cu

---
+++ linux_cuda/mouse.cu
@@ -0,0 +1,83 @@
+/*
+ * mouse.cu
+ */
+
+#include "mouse.h"
+#include <stdio.h>	// debug
+#include <math.h>
+#include "common.h"
+#include "vector3d.h"
+#include "quaternion.h"
+
+Quaternion q, qstart;
+double radius = 0.4 * HEIGHT;
+int fDraw = false;
+int input_changed = false;
+
+Vector3d p0, p1;
+
+void pick(Vector3d *p, int x, int y)
+{
+        p->x = (x - HEIGHT / 2) / radius;
+        p->y = (y - WIDTH / 2) / radius;
+        p->z = 0;
+        double r = p->x*p->x + p->y*p->y;
+        if(r > 1)
+        {
+                double s = 1 / sqrt(r);
+                p->x *= s;
+                p->y *= s;
+        }
+        else
+        {
+                p->z = sqrt(1 - r);
+        }
+}
+
+void mouse(char op, int x, int y)
+{
+	switch(op)
+	{
+
+		case 'd':
+		{
+			fDraw = true;
+			pick(&p0, x, y);
+			qstart.x = direction.x;
+			qstart.y = direction.y;
+			qstart.z = direction.z;
+			qstart.w = 0;
+			break;
+		}
+
+		case 'u':
+			fDraw = false;
+			break;
+
+		case 'm':
+			if(fDraw && !input_changed)
+			{
+				pick(&p1, x, y);
+				Vector3d p2;
+				cross3d(p0, p1, &p2);
+				q.x = p2.x;
+				q.y = p2.y;
+				q.z = p2.z;
+				q.w = dot3d(p0, p1);
+				//
+				Quaternion qnow;
+				mul(&qnow, q, qstart);
+				_direction.x = qnow.x;
+				_direction.y = qnow.y;
+				_direction.z = qnow.z;
+				norm3d(&_direction);
+				double hy = sqrt(position.x * position.x + position.y * position.y + position.z * position.z);
+				_position.x = -_direction.x * hy;
+				_position.y = -_direction.y * hy;
+				_position.z = -_direction.z * hy;
+				input_changed = true;
+			}
+			break;
+	}
+}
+
