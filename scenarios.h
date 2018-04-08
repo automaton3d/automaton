2df180e394819f6dfd5e498b5777a12faba8f483
automaton3d <34599064+automaton3d@users.noreply.github.com>
2018-04-08 18:49:30 -0300

    Update scenarios.h

--- scenarios.h
+++ scenarios.h
@@ -5,12 +5,22 @@
 #ifndef SCENARIOS_H_
 #define SCENARIOS_H_
 
+#include <windows.h>
+
+#define HEL	1
+#define ELT	2
+#define CHI	4
+#define GRV	8
+#define CLR	16
+
+extern boolean showGrid, showBox, showAxes;
+
 /// Functions ///
 
 void BurstScenario();
 void VacuumScenario();
-void VIRTScenario();
-void REALScenario();
+void NoUXUScenario();
+void UScenario();
 void UXUScenario();
 void GRAVScenario();
 void SEEDScenario();
