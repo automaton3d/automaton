79c6aaf62403150b616f78d2a4cc24e12198ca82
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2018-04-28 20:00:04 -0300

    Update brick.c

--- brick.c
+++ brick.c
@@ -20,16 +20,26 @@ void copyBrick(Brick *dst, Brick *org)
 	dst->p19 = w;
 }
 
+/*
+ * Preserves p0, p1, p18, p19.
+ */
 void cleanBrick(Brick *t)
 {
 	Tuple p0 = t->p0;
+	unsigned p1 = t->p1 % SYNCH;
 	unsigned p18 = t->p18;
 	unsigned short w = t->p19;
 	memset(t, 0, sizeof(Brick));
 	t->p0 = p0;
+	t->p1 = p1;
 	t->p15.x = -1;
 	t->p18 = p18;
 	t->p19 = w;
+}
+
+boolean isColored(Brick *b)
+{
+	return b->p9 != LEPT && b->p9 != ANTILEPT;
 }
 
 char *brick2str(Brick *t)
