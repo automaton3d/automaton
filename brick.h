e7e9ab391acd0262826a3b6121c5e4e71c14527d
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2018-05-23 19:27:42 -0300

    Update brick.h

--- brick.h
+++ brick.h
@@ -34,15 +34,12 @@
 #define U			1		// unpaired
 #define P			2		// pair
 
-#define PXP			3
-#define UXU			4
-#define UXP			5
-#define UXG			6
-#define REISSUE		7
-#define ANNIHIL		8
-#define VACUUM		9
-#define INERTIA		10
-#define EM_BOSON	11
+#define PXP			4
+#define UXU			8
+#define UXP			16
+#define UXG			32
+#define REISSUE		64
+#define VACUUM		128
 
 // p16
 
@@ -84,7 +81,8 @@ typedef struct
 	Tuple			p15;	// return cell
 	unsigned char	p16;	// cohesion
 	unsigned		p17;	// virtual decay
-	unsigned		p18;	// disambiguation
+	char			p29;	// gravity excitation
+	unsigned		p30;	// directionality
 	//
 	// Auxiliary
 	//
@@ -99,6 +97,7 @@ typedef struct
 	unsigned char	p27;	// burst direction
 	double 			p28a1;	// a1
 	double			p28a2;	// a2
+	unsigned		p18;	// disambiguation
 
 } Brick;
 
