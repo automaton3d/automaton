df2ef1b1a78147cd875dfdee2ed15bc4ea9461cb
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2018-05-23 19:29:01 -0300

    Update brick.h

--- brick.h
+++ brick.h
@@ -1,113 +1,37 @@
 /*
- * brick.h
+ * common.h
  */
 
-#ifndef BRICK_H_
-#define BRICK_H_
+#ifndef COMMON_H_
+#define COMMON_H_
 
-#include "tuple.h"
+#define true 1
+#define false 0
+#define null 0
 
-// General code
+#define WIDTH   800
+#define HEIGHT  800
 
-#define UNDEF		0
+// Colors
 
-// p8 gravity
+#define BLK     0
+#define WHT     1
+#define RR		2
+#define GG		3
+#define BB		4
+#define GRAY    5
+#define BOX		6
 
-#define REAL		+1
-#define VIRTUAL		-1
+// Special keys
 
-// p9 color
+#define ESC		27
 
-#define RED    		0x20
-#define GREEN  		0x10
-#define BLUE   		0x08
-#define ANTIRED    	0x04
-#define ANTIGREEN  	0x02
-#define ANTIBLUE   	0x01
-#define WHITE		(RED | GREEN | BLUE)
-#define ANTIWHITE	(ANTIRED | ANTIGREEN | ANTIBLUE)
-#define LEPT		0x38
-#define ANTILEPT	0x07
+// Macros
 
-// p13 preon interactions
+#define pwm(n) (n%SQRT < n/SQRT)
 
-#define U			1		// unpaired
-#define P			2		// pair
+// Other symbols
 
-#define PXP			4
-#define UXU			8
-#define UXP			16
-#define UXG			32
-#define REISSUE		64
-#define VACUUM		128
+#define NSCENES	20
 
-// p16
-
-#define FREE		0
-#define BOUND		1
-
-// p21 status
-
-#define	PREON		0X01
-#define GRAV		0X04
-#define ORG			0x08
-
-// p25 burst type
-
-#define DESTROY		1
-#define CANCEL		2
-
-/*
- * Preon structure
- */
-typedef struct
-{
-	Tuple			p0;		// xyz position
-	unsigned		p1;		// clock
-	Tuple			p2;		// origin vector
-	Tuple			p3;		// LM direction
-	Tuple			p4;		// spin direction
-	char			p5;		// helicity
-	char			p6;		// electric charge
-	char			p6s;	// static force
-	char			p7;		// chirality
-	char			p8;		// gravity charge
-	unsigned char	p9;		// color and conjugation
-	unsigned		p10;	// entanglement
-	unsigned char	p11;	// sinusoid pwm
-	int 			p12;	// frequency
-	unsigned char	p13;	// interaction
-	int				p14;	// interference
-	Tuple			p15;	// return cell
-	unsigned char	p16;	// cohesion
-	unsigned		p17;	// virtual decay
-	char			p29;	// gravity excitation
-	unsigned		p30;	// directionality
-	//
-	// Auxiliary
-	//
-	unsigned		p19;	// w address
-	unsigned		p20;	// w of peer
-	unsigned char	p21;	// status
-	unsigned char	p22;	// is pair flag
-	unsigned char	p23;	// wavefront direction
-	unsigned		p24;	// wavefront synch: t1
-	unsigned char	p25;	// messenger code
-	Tuple			p26;	// burst origin vector;
-	unsigned char	p27;	// burst direction
-	double 			p28a1;	// a1
-	double			p28a2;	// a2
-	unsigned		p18;	// disambiguation
-
-} Brick;
-
-// Functions declarations
-
-char *brick2str(Brick *t);
-void copyBrick(Brick *dst, Brick *org);
-void cleanBrick(Brick *t);
-unsigned signature(Brick *b);
-boolean isColored(Brick *b);
-Tuple getOrg(Brick *b);
-
-#endif /* PREON_H_ */
+#endif /* COMMON_H_ */
