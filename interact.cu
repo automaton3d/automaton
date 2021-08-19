cff10bfcbfcaf7600baf3671fed3da1e1262306c
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2021-08-19 09:09:23 -0300

    Update interact.cu

--- interact.cu
+++ interact.cu
@@ -4,264 +4,285 @@
 #include <stdlib.h>
 #include "automaton.h"
 
-__global__ void interact(struct Cell* lattice)
+__global__ void interact(Cell* lattice)
 {
 	int id = blockIdx.x * blockDim.x + threadIdx.x;
 	if (id < SIDE3)
 	{
-		struct Cell* cell = lattice + id;
-		int sig1 = cell->h->c == cell->c && cell->c == 0 && cell->h->q == cell->q && cell->q == 1 && cell->h->d == cell->d && cell->d == 0;
-		int sig2 = cell->h->c == cell->c && cell->c == 7 && cell->h->q == cell->q && cell->q == 0 && cell->h->d == cell->d && cell->d == 1;
-		int sig3 = cell->h->c == cell->c && cell->c != 0 && cell->c != 7 && cell->h->q == cell->q;
-		//
-		int c1 = cell->h->q == cell->q && cell->h->w == cell->w && sig1 == sig2;
-		int c2 = cell->h->q != cell->q && cell->h->w != cell->w && sig1 != sig2;
-		int c3 = cell->h->d == 0 && cell->h->w == 0 && cell->h->w != cell->w;
-		int c4 = cell->h->d == 1 && cell->h->w == 1 && cell->h->w != cell->w;
-		//
-		// Play pseudo dices
-		//
-		if (cell->h->noise > abs(cell->h->phi) && cell->noise > abs(cell->phi) && (!ISNULL(cell->h->p) || !ISNULL(cell->p)))
+		Cell* cell = lattice + id;
+		Cell *active_stack, *passive_stack;
+		if (cell->active)
 		{
-			if (cell->h->d == cell->d)
+			active_stack = cell;
+			passive_stack = cell + SIDE3 * SIDE2;
+		}
+		else
+		{
+			passive_stack = cell;
+			active_stack = cell + SIDE3 * SIDE2;
+		}
+		for (int v = 0; v < 1/*SIDE2*/; v++)
+		{
+			int sig1 = ((active_stack->charge ^ passive_stack->charge) & C_MASK) == 0 && (passive_stack->charge & C_MASK) == 0 &&
+				((active_stack->charge ^ passive_stack->charge) & Q_MASK) == 0 && (passive_stack->charge & Q_MASK) == Q_MASK && 
+				((active_stack->charge ^ passive_stack->charge) & D_MASK) == 0 && (passive_stack->charge & D_MASK) == 0;
+			int sig2 = ((active_stack->charge ^ passive_stack->charge) & C_MASK) == 0 &&
+				(passive_stack->charge & C_MASK) == C_MASK && ((active_stack->charge ^ passive_stack->charge) & Q_MASK) == 0 && 
+				(passive_stack->charge & Q_MASK) == 0 && ((active_stack->charge ^ passive_stack->charge) & D_MASK) == 0 && (passive_stack->charge & D_MASK) == D_MASK;
+			int sig3 = ((active_stack->charge ^ passive_stack->charge) & C_MASK) == 0 && (passive_stack->charge & C_MASK) != 0 && (passive_stack->charge & C_MASK) != C_MASK &&
+				((active_stack->charge ^ passive_stack->charge) & Q_MASK) == 0;
+			//
+			int c1 = ((active_stack->charge ^ passive_stack->charge) & Q_MASK) == 0 && ((active_stack->charge ^ passive_stack->charge) & W_MASK) == 0 && sig1 == sig2;
+			int c2 = ((active_stack->charge ^ passive_stack->charge) & Q_MASK) != 0 && ((active_stack->charge ^ passive_stack->charge) & W_MASK) != 0 && sig1 != sig2;
+			int c3 = (active_stack->charge & D_MASK) == 0 && (active_stack->charge & W_MASK) == 0 && ((active_stack->charge ^ passive_stack->charge) & W_MASK) != 0;
+			int c4 = (active_stack->charge & D_MASK) == D_MASK && (active_stack->charge & W_MASK) == W_MASK && ((active_stack->charge ^ passive_stack->charge) & W_MASK) != 0;
+			//
+			if(ISNULL(active_stack->pole))
+				COPY(passive_stack->pole, active_stack->p);
+			//
+			// Play pseudo dices
+			//
+			if (false)//active_stack->noise > abs(active_stack->phi) && passive_stack->noise > abs(passive_stack->phi) && (!ISNULL(active_stack->p) || !ISNULL(passive_stack->p)))
 			{
-				// Same sector?
-				//
-				if (cell->h->f == 1 && cell->f == 1)
+				if (((active_stack->charge ^ passive_stack->charge) & D_MASK) == 0)
 				{
-					// F x F
+					// Same sector?
 					//
-					if (cell->h->q != cell->q)
+					if (active_stack->f == 1 && passive_stack->f == 1)
 					{
-						// Annihilation?
+						// F x F
 						//
-						cell->h->b = (cell->h->b * cell->b) % SIDE2;
-						cell->b = cell->h->b;
-						//
-						// Reissue R1 and R2 from this
-						//
-						RESET(cell->h->pole);
-						RESET(cell->pole);
-					}
-					else if (sig1 || sig2 || sig3)
-					{
-						// Similar?
-						//
-						// Cohesion
-						//
-						if (cell->h->b != cell->b)
+						if (((active_stack->charge ^ passive_stack->charge) & Q_MASK) != 0)
 						{
-							cell->h->b = (cell->h->b * cell->b) % SIDE2;
-							cell->b = cell->h->b;
+							// Annihilation?
+							//
+							active_stack->b = (active_stack->b * passive_stack->b) % SIDE2; // ??? erro ???
+							passive_stack->b = active_stack->b;
+							//
+							// Reissue R1 and R2 from this
+							//
+							RESET(active_stack->pole);// ???
+							COPY(passive_stack->pole, active_stack->p);
 						}
-						//
-						// s1 <-> s2
-						//
-						int temp;
-						temp = cell->h->s[0];
-						cell->s[0] = cell->h->s[0];
-						cell->h->s[0] = temp;
-						temp = cell->h->s[1];
-						cell->s[1] = cell->h->s[1];
-						cell->h->s[1] = temp;
-						temp = cell->h->s[2];
-						cell->s[2] = cell->h->s[2];
-						cell->h->s[2] = temp;
-						//
-						// Reissue R1 from pole(R1) and R2 from pole(R2)
-						//
-						RESET(cell->h->o);
-						RESET(cell->o);
-					}
-				}
-				else if (cell->h->f > 1 && cell->f > 1)
-				{
-					// B x B
-					//
-					if (cell->h->c == ~cell->c && cell->h->code == cell->code && cell->code == GLUON)
-					{
-						// gluon-gluon?
-						//
-						// Swap colors
-						//
-						int temp = cell->h->c;
-						cell->h->c = cell->c;
-						cell->c = temp;
-						//
-						// Reissue R1 from pole(R1)
-						//
-						RESET(cell->h->o);
-					}
-					else if (!ISNULL(cell->h->p) && !ISNULL(cell->p))
-					{
-						if (c1 || c2 || c3 || c4)
+						else if (sig1 || sig2 || sig3)
 						{
-							// chiral?
+							// Similar?
+							//
+							// Cohesion
+							//
+							if (active_stack->b != passive_stack->b)
+							{
+								active_stack->b = (active_stack->b * passive_stack->b) % SIDE2;
+								passive_stack->b = active_stack->b;
+							}
+							//
+							// s1 <-> s2
+							//
+							int temp;
+							temp = active_stack->s[0];
+							passive_stack->s[0] = active_stack->s[0];
+							active_stack->s[0] = temp;
+							temp = active_stack->s[1];
+							passive_stack->s[1] = active_stack->s[1];
+							active_stack->s[1] = temp;
+							temp = active_stack->s[2];
+							passive_stack->s[2] = active_stack->s[2];
+							active_stack->s[2] = temp;
+							//
+							// Reissue R1 from pole(R1) and R2 from pole(R2)
+							//
+							RESET(active_stack->o);
+							RESET(passive_stack->o);	//???
+							COPY(passive_stack->pole, active_stack->p); // ???
+						}
+					}
+					else if (active_stack->f > 1 && passive_stack->f > 1)
+					{
+						// B x B
+						//
+						if (((active_stack->charge ^ ~passive_stack->charge) & C_MASK) == 0 && active_stack->code == passive_stack->code && passive_stack->code == GLUON)
+						{
+							// gluon-gluon?
+							//
+							// Swap colors
+							//
+							int temp = active_stack->charge & C_MASK;
+							active_stack->charge &= ~C_MASK;
+							active_stack->charge |= (passive_stack->charge & C_MASK);
+							passive_stack->charge &= ~C_MASK;
+							passive_stack->charge |= temp;
+							//
+							// Reissue R1 from pole(R1)
+							//
+							RESET(active_stack->o);
+							passive_stack->d0 = 0;	// replicar !!!
+							passive_stack->t = 0;	// replicar !!!
+						}
+						else if (!ISNULL(active_stack->p) && !ISNULL(passive_stack->p))
+						{
+							if (c1 || c2 || c3 || c4)
+							{
+								// chiral?
+								//
+								// Reissue R1 and R2 from cp1
+								//
+								passive_stack->b = active_stack->b;
+								RESET(active_stack->o);
+								//
+								// Reissue R2 from cp1
+								//
+								RESET(passive_stack->o);
+							}
+							else
+							{
+								// TODO
+							}
+						}
+						else if (sig1 != 0 && sig1 != 3 && sig2 != 0 && sig2 != 3)
+						{
+							int temp = active_stack->charge & C_MASK;
+							active_stack->charge &= ~C_MASK;
+							active_stack->charge |= (passive_stack->charge & C_MASK);
+							passive_stack->charge &= ~C_MASK;
+							passive_stack->charge |= temp;
+							passive_stack->b = active_stack->b;
 							//
 							// Reissue R1 and R2 from cp1
 							//
-							cell->b = cell->h->b;
-							RESET(cell->h->o);
+							passive_stack->b = active_stack->b;
+							RESET(active_stack->o);
+							RESET(passive_stack->o);
+						}
+					}
+					if (active_stack->f == 1 && passive_stack->f > 1)
+					{
+						// F x B
+						//
+						if ((active_stack->charge & C_MASK) != 0 && (active_stack->charge & C_MASK) != C_MASK && (passive_stack->charge & C_MASK) != 0 && 
+							(passive_stack->charge & C_MASK) != C_MASK)
+						{
+							int temp = active_stack->charge & C_MASK;
+							active_stack->charge &= ~C_MASK;
+							active_stack->charge |= (passive_stack->charge & C_MASK);
+							passive_stack->charge &= ~C_MASK;
+							passive_stack->charge |= temp;
+							passive_stack->b = active_stack->b;
 							//
-							// Reissue R2 from cp1
+							// Reissue R1 from pole(R1) and R2 from pole(R2)
 							//
-							RESET(cell->o);
+							RESET(active_stack->o);
+							RESET(passive_stack->o);
 						}
 						else
 						{
-							// TODO
+							passive_stack->b = active_stack->b;
+							//
+							// Reissue R1 and R2 from this
+							//
+							RESET(active_stack->pole);
+							RESET(active_stack->o);
+							RESET(passive_stack->pole);
+							RESET(passive_stack->o);
 						}
 					}
-					else if (sig1 != 0 && sig1 != 3 && sig2 != 0 && sig2 != 3)
+					else if (active_stack->f > 1 && passive_stack->f == 1)
 					{
-						int temp = cell->h->c;
-						cell->h->c = cell->c;
-						cell->c = temp;
-						cell->b = cell->h->b;
+						// B x F
 						//
-						// Reissue R1 and R2 from cp1
+						if ((active_stack->charge & C_MASK) != 0 && (active_stack->charge & C_MASK) != C_MASK && (passive_stack->charge & C_MASK) != 0 && 
+							(passive_stack->charge & C_MASK) != C_MASK)
+						{
+							int temp = active_stack->charge & C_MASK;
+							active_stack->charge &= ~C_MASK;
+							active_stack->charge |= (passive_stack->charge & C_MASK);
+							passive_stack->charge &= ~C_MASK;
+							passive_stack->charge |= temp;
+							//
+							// Reissue R1 from pole(R1) and R2 from pole(R2)
+							//
+							RESET(active_stack->o);
+							RESET(passive_stack->o);
+						}
+						else
+						{
+							passive_stack->b = active_stack->b;
+							//
+							// Reissue R1 and R2 from this
+							//
+							RESET(active_stack->pole);
+							RESET(passive_stack->pole);
+						}
+					}
+					else if (active_stack->b == passive_stack->b)
+					{
+						// Messenger interactions
 						//
-						cell->b = cell->h->b;
-						RESET(cell->h->o);
-						RESET(cell->o);
+						if (!ISNULL(active_stack->p))
+						{
+							// REISSUE(active_stack, POLE(active_stack))
+							//
+							RESET(active_stack->pole);
+							//
+							// REISSUE(passive_stack, TRANSPORT(passive_stack, active_stack));
+							//
+							passive_stack->pole[0] = active_stack->o[0] - passive_stack->o[0];
+							passive_stack->pole[1] = active_stack->o[1] - passive_stack->o[1];
+							passive_stack->pole[2] = active_stack->o[2] - passive_stack->o[2];
+						}
+						else
+						{
+							// REISSUE(passive_stack, POLE(passive_stack));
+							//
+							RESET(passive_stack->pole);
+							//
+							// REISSUE(active_stack, TRANSPORT(active_stack, passive_stack));
+							//
+							active_stack->pole[0] = passive_stack->o[0] - active_stack->o[0];
+							active_stack->pole[1] = passive_stack->o[1] - active_stack->o[1];
+							active_stack->pole[2] = passive_stack->o[2] - active_stack->o[2];
+						}
 					}
 				}
-				if (cell->h->f == 1 && cell->f > 1)
+				else
 				{
-					// F x B
+					// Inter-sector
 					//
-					if (cell->h->c != 0 && cell->h->c != 7 && cell->c != 0 && cell->c != 7)
+					if ((((active_stack->charge & D_MASK) == 0 && sig1 == 2) || ((active_stack->charge & D_MASK) == 1 && sig1 == 3)))
 					{
-						int temp = cell->h->c;
-						cell->h->c = cell->c;
-						cell->c = temp;
-						cell->b = cell->h->b;
-						//
-						// Reissue R1 from pole(R1) and R2 from pole(R2)
-						//
-						RESET(cell->h->o);
-						RESET(cell->o);
-					}
-					else
-					{
-						cell->b = cell->h->b;
+						int temp = active_stack->charge & C_MASK;
+						active_stack->charge &= ~C_MASK;
+						active_stack->charge |= (passive_stack->charge & C_MASK);
+						passive_stack->charge &= ~C_MASK;
+						passive_stack->charge |= temp;
 						//
 						// Reissue R1 and R2 from this
 						//
-						RESET(cell->h->pole);
-						RESET(cell->h->o);
-						RESET(cell->pole);
-						RESET(cell->o);
+						RESET(active_stack->pole);
+						RESET(passive_stack->pole);
 					}
-				}
-				else if (cell->h->f > 1 && cell->f == 1)
-				{
-					//
-					// B x F
-					//
-					if (cell->h->c != 0 && cell->h->c != 7 && cell->c != 0 && cell->c != 7)
+					else if (c1 || c2 || c3 || c4)
 					{
-						int temp = cell->h->c;
-						cell->h->c = cell->c;
-						cell->c = temp;
-						cell->b = cell->h->b;
+						// Chiral?
 						//
-						// Reissue R1 from pole(R1) and R2 from pole(R2)
-						//
-						RESET(cell->h->o);
-						RESET(cell->o);
-					}
-					else
-					{
-						cell->b = cell->h->b;
+						int temp = active_stack->charge & W_MASK;
+						active_stack->charge &= ~W_MASK;
+						active_stack->charge |= (passive_stack->charge & W_MASK);
+						passive_stack->charge &= ~W_MASK;
+						passive_stack->charge |= temp;
 						//
 						// Reissue R1 and R2 from this
 						//
-						RESET(cell->h->pole);
-						RESET(cell->pole);
-					}
-				}
-				else if (cell->h->b == cell->b)
-				{
-					// Messenger interactions
-					//
-					if (!ISNULL(cell->h->p))
-					{
-						// REISSUE(cell->h, POLE(cell->h))
-						//
-						RESET(cell->h->pole);
-						//
-						// REISSUE(cell, TRANSPORT(cell, cell->h));
-						//
-						cell->pole[0] = cell->h->o[0] - cell->o[0];
-						cell->pole[1] = cell->h->o[1] - cell->o[1];
-						cell->pole[2] = cell->h->o[2] - cell->o[2];
-					}
-					else
-					{
-						// REISSUE(cell, POLE(cell));
-						//
-						RESET(cell->pole);
-						//
-						// REISSUE(cell->h, TRANSPORT(cell->h, cell));
-						//
-						cell->h->pole[0] = cell->o[0] - cell->h->o[0];
-						cell->h->pole[1] = cell->o[1] - cell->h->o[1];
-						cell->h->pole[2] = cell->o[2] - cell->h->o[2];
+						RESET(active_stack->pole);
+						RESET(passive_stack->pole);
 					}
 				}
 			}
-			else
-			{
-				// Inter-sector
-				//
-				if (((cell->h->d == 0 && sig1 == 2) || (cell->h->d == 1 && sig1 == 3)))
-				{
-					int temp = cell->h->c;
-					cell->h->c = cell->c;
-					cell->c = temp;
-					//
-					// Reissue R1 and R2 from this
-					//
-					RESET(cell->h->pole);
-					RESET(cell->pole);
-				}
-				else if (c1 || c2 || c3 || c4)
-				{
-					// Chiral?
-					//
-					int temp = cell->h->w;
-					cell->h->w = cell->w;
-					cell->w = temp;
-					//
-					// Reissue R1 and R2 from this
-					//
-					RESET(cell->h->pole);
-					RESET(cell->pole);
-				}
-			}
+			active_stack->t = 0;
+			passive_stack->t = 0;
+			active_stack->synch = -1;
+			passive_stack->synch = -1;
+			active_stack = nextV(active_stack);
+			passive_stack = nextV(passive_stack);
 		}
 	}
-	/*
-	else {
-		if ((cell->t / LIGHT) % 2 == 1 && cell->t > 1 && cell->t < TMAX) {
-			//
-			// Shift 'vertically'
-			//
-			cell->h->h = cell->h->v->h;
-			cell->h->v = cell->h->v->v;
-		}
-	}
-	//
-	// Update time counter
-	//
-	if (cell->t <= LIGHT)
-		cell->t--;
-	else
-		cell->t -= LIGHT;
-	if (cell->t == 0) {
-		cell->t = TMAX;
-		cell->active = !cell->active;
-	}
-	*/
 }
