417f22547d8838eade28382ecd676a1c4e08d816
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2021-08-19 09:13:42 -0300

    Update commute.cu

--- commute.cu
+++ commute.cu
@@ -4,37 +4,43 @@
 #include <stdlib.h>
 #include "automaton.h"
 
-__global__ void commute(struct Cell* lattice)
+__global__ void commute(Cell* lattice)
 {
 	int id = blockIdx.x * blockDim.x + threadIdx.x;
 	if (id < SIDE3)
 	{
-		struct Cell* cell = lattice + id;
-		if (!cell->active)
-			cell = cell->h;
-		//
+		Cell* cell = lattice + id;
+		Cell* passive_stack, * active_stack;
+		if (cell->active)
+		{
+			active_stack = cell;
+			passive_stack = cell + SIDE3 * SIDE2;
+		}
+		else
+		{
+			passive_stack = cell;
+			active_stack = cell + SIDE3 * SIDE2;
+		}
 		for (int v = 0; v < SIDE2; v++)
 		{
-			cell->active = false;
-			cell->h->active = true;
-			cell->h->q = cell->q;
-			cell->h->w = cell->w;
-			cell->h->c = cell->c;
-			cell->h->d = cell->d;
-			COPY(cell->h->o, cell->o);
-			COPY(cell->h->p, cell->p);
-			COPY(cell->h->s, cell->s);
-			COPY(cell->h->pole, cell->pole);
-			cell->h->code = 0;
-			cell->h->noise ^= cell->noise;
-			cell->h->f = cell->f;
-			cell->h->b = cell->b;
-			if (ISNULL(cell->o) && !ISNULL(cell->p))
+			active_stack->active = false;
+			passive_stack->active = true;
+			active_stack->charge = passive_stack->charge;
+			COPY(active_stack->o, passive_stack->o);
+			COPY(active_stack->p, passive_stack->p);
+			COPY(active_stack->s, passive_stack->s);
+			COPY(active_stack->pole, passive_stack->pole);
+			active_stack->code = 0;
+			active_stack->noise ^= passive_stack->noise;
+			active_stack->f = passive_stack->f;
+			active_stack->b = passive_stack->b;
+			if (ISNULL(passive_stack->o) && !ISNULL(passive_stack->p))
 			{
-				cell->h->f = 1;
-				cell->h->b = 0;
+				active_stack->f = 1;
+				active_stack->b = 0;
 			}
-			cell = cell->v;
+			passive_stack = nextV(passive_stack);
+			active_stack = nextV(active_stack);
 		}
 	}
 }
