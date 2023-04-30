059f7f5574a516dda7fc3e9052de8fa0cc3ea4fe
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2023-04-29 21:07:19 -0300

    Add files via upload

--- CDT/test/printCell.c
+++ CDT/test/printCell.c
@@ -11,23 +11,24 @@
 boolean first = true;
 void printCell(Cell *cell)
 {
-	if(cell->off == 0)
+	if(cell->offE == 0)
 	{
 		if(first)
 		{
-			puts("\nn\tch\toff\ta\tpos\tk\tsyn\tnoise\to\tp");
+			puts("\nn\tch\tpos\toff\ta\tk\tsyn\tnoise\to\tp\tpo");
 			puts("---------------------------------------------------------------------------------");
 		}
 		printf("%d\t", cell->n);
-		printf("%d,%d,%d\t", cell->pos[0], cell->pos[1], cell->pos[2]);
 		printf("0x%02x\t", cell->ch);
-		printf("%d\t", cell->off);
+		printf("%d\t", cell->offL);
+		printf("%d\t", cell->offE);
 		printf("%d\t", cell->a);
 		printf("%d\t", cell->k);
 		printf("%d\t", cell->syn);
 		printf("%d\t", cell->r);
 		printf("%d,%d,%d\t", cell->o[0], cell->o[1], cell->o[2]);
-		printf("%d,%d,%d\n", cell->p[0], cell->p[1], cell->p[2]);
+		printf("%d,%d,%d\t", cell->p[0], cell->p[1], cell->p[2]);
+		printf("%d,%d,%d\n", cell->po[0], cell->po[1], cell->po[2]);
 		first = false;
 	}
 }
--- CDT/test/test-utils.c
+++ CDT/test/test-utils.c
@@ -17,17 +17,17 @@ Cell *isSingular(Cell *latt)
 {
 	Cell *last;
 	boolean first = true;
-	int unique[3];
+	int unique;
     for(int i = 0; i < SIDE3 * SIDE3; i++, latt++)
     {
     	if(latt->k == EMPTY && latt->k != EMPTY)
     	{
     		if(first)
     		{
-    			CP(unique, latt->pos);
+    			unique = latt->offL;
     			first = false;
     		}
-    		if(!EQ(unique, latt->pos))
+    		if(unique != latt->offL)
     			return false;
     		last = latt;
     	}
@@ -60,7 +60,7 @@ Cell *huntMomentum(Cell *latt)
 {
     for(int i = 0; i < SIDE3 * SIDE3; i++, latt++)
     {
-    	if(!ZERO(latt->p) && latt->off == 0)
+    	if(!ZERO(latt->p) && latt->offE == 0)
     	{
     		return latt;
     	}
