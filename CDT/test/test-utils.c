2f5c8d09d75b568bacc4e4392776daef38a849d7
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2023-06-26 09:08:16 -0300

    Add files via upload

--- CDT/test/test-utils.c
+++ CDT/test/test-utils.c
@@ -7,6 +7,8 @@
 #include "test.h"
 #include "../simulation.h"
 
+extern Cell *latt1;
+
 int countMomentum(Cell *latt)
 {
 	int n = 0;
@@ -14,6 +16,20 @@ int countMomentum(Cell *latt)
     {
     	if(!ZERO(latt->p))
     		n++;
+    }
+    return n;
+}
+
+int countMomentum__(Cell *latt)
+{
+	int n = 0;
+    for(int i = 0; i < SIDE6; i++, latt++)
+    {
+    	if(!ZERO(latt->p))
+    	{
+    		n++;
+    		return 1;
+    	}
     }
     return n;
 }
@@ -27,4 +43,34 @@ boolean sanity(Cell *latt)
 			n++;
 	}
 	return n == SIDE6 - SIDE3;
+}
+
+void printCoords(Cell *c)
+{
+    int x = c->off % SIDE2;
+    int y = (c->off / SIDE2) % SIDE2;
+    int z = (c->off / SIDE4) % SIDE2;
+    printf("(%d,%d,%d)\n", x, y, z); fflush(stdout);
+}
+
+int main___()
+{
+	initSimulation();
+	Cell *c0 = latt1 + SIDE_2 * (1 + SIDE2 + SIDE4);
+	printCoords(c0);
+	printf("c0: %d\n", c0->off); fflush(stdout);
+	assert(isCentralPoint(c0->off));
+	for (int i = 0; i < 100; i++)
+	{
+		int dir = rand() % 6;
+		printf("\ndir=%d\n", dir);
+		for(int i = 0; i < SIDE2; i++)
+		{
+			c0 = neighbor(c0, dir);
+			printCoords(c0);
+		}
+		assert(isCentralPoint(c0->off));
+	}
+	printf("If ==(%d,%d,%d) then Ok.\n", SIDE_2, SIDE_2, SIDE_2);
+	return 0;
 }
