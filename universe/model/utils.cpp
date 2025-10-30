1870dfa96996dc312c6ca03ac5df526f7c87364c
Alexandre F. Neto <34599064+automaton3d@users.noreply.github.com>
2025-10-30 20:10:50 -0300

    Add files via upload

--- universe/model/utils.cpp
+++ universe/model/utils.cpp
@@ -12,6 +12,13 @@
 #include "../GUIrenderer.h"
 #include "simulation.h"
 
+namespace framework
+{
+  extern Tickbox* tomo;
+  extern std::vector<Radio> tomoDirs;
+  extern unsigned tomo_x, tomo_y, tomo_z;
+}
+
 namespace automaton
 {
   extern unsigned EL;
@@ -28,25 +35,37 @@ namespace automaton
   void updateBuffer()
   {
     int w = framework::list->getSelected();
-    // Iterate over the 3D grid to update the voxel data
     unsigned index3D = 0;
+
     for (unsigned x = 0; x < EL; x++)
     {
       for (unsigned y = 0; y < EL; y++)
       {
         for (unsigned z = 0; z < EL; z++)
         {
+          bool visible = true;
+          // Apply tomography filter if enabled
+          if (framework::tomo && framework::tomo->getState())
+          {
+            if (framework::tomoDirs[0].isSelected())      // XY → fix Z
+              visible = (z == framework::tomo_z);
+            else if (framework::tomoDirs[1].isSelected()) // YZ → fix X
+              visible = (x == framework::tomo_x);
+            else if (framework::tomoDirs[2].isSelected()) // ZX → fix Y
+              visible = (y == framework::tomo_y);
+          }
+          if (!visible)
+          {
+            voxels[index3D++] = RGB(0, 0, 0);  // Black for hidden voxels
+            continue;
+          }
           Cell &cell = getCell(lattice_curr, x, y, z, w);
           if (cell.t == cell.d)
           {
             if (cell.a == W_DIM)
-            {
               voxels[index3D] = RGB(255, 0, 0); // Red (orphan)
-            }
             else
-            {
               voxels[index3D] = RGB(255, 255, 255); // White (wavefront match)
-            }
           }
           else if (cell.cB)
           {
@@ -56,6 +75,7 @@ namespace automaton
           {
             voxels[index3D] = RGB(0, 0, 0); // Black
           }
+
           index3D++;
         }
       }
