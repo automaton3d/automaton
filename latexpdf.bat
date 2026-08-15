03dc7d442b625995e4aee48153a3b1f6b0c2cb4f
A.F. Neto <javaresende@gmail.com>
2026-08-15 14:44:09 -0300

    Updated latexpdf.bat

--- latexpdf.bat
+++ latexpdf.bat
@@ -1,4 +1,9 @@
+cd /d E:\automaton
+git fetch origin
+git reset --hard origin/devin/polar-phase
+git clean -fd
 pdflatex manuscript.tex
 biber manuscript
 pdflatex manuscript.tex
 pdflatex manuscript.tex
+manuscript.pdf
